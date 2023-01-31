41382f449a8e707d1e07f64ce26e33abd608129d
Frederik Darling <frederikdarling@live.dk>
2023-01-31 15:52:55 +0100

    adding components

--- pokemon-trainer/src/app/app.module.ts
+++ pokemon-trainer/src/app/app.module.ts
@@ -4,12 +4,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPage } from './pages/login/login.page';
 import { LoginFormComponent } from './components/login-form/login-form.component';
+import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
+import { CataloguePage } from './pages/catalogue/catalogue.page';
+import { TrainerProfileComponent } from './pages/trainer-profile/trainer-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPage,
-    LoginFormComponent
+    LoginFormComponent,
+    PokemonListComponent,
+    CataloguePage,
+    TrainerProfileComponent
   ],
   imports: [
     BrowserModule,
